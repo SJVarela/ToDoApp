e15241beaacc1e3fcf6321989e8e48fe8c180d88
Santiago Varela <s.javiervarela@gmail.com>
2019-03-14 10:54:27 -0300

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+using ToDoApp.Services;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly IToDoItemService _toDoItemService;
+        public ToDoController(IToDoItemService toDoItemService)
+        {
+            _toDoItemService = toDoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _toDoItemService.GetIncompleteItemsAsync();
+            var model = new ToDoViewModel()
+            {
+                Items = items
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Services/IToDoItemService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    public interface IToDoItemService
+    {
+        Task<ToDoItem[]> GetIncompleteItemsAsync();
+    }
+}
